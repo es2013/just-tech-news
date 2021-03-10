d9e9d18c85f1fa7c4b1c8e84038fea01e04ecd4d
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-03-09 20:10:16 -0800

    added model joins for post and user models and completed crud application for post

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./api/user-routes.js');
+const postRoutes = require('./api/post-routes');
 //collects endpoints and prefixes
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
 
