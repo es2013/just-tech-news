887c7d8dec5583622e463d2f11c3fd1d6287ffbe
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-03-13 21:16:30 -0800

    added login and logout routes and session dependencies

--- server.js
+++ server.js
@@ -5,9 +5,21 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+//session libraries
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +27,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
+
 
 
 // turn on routes
